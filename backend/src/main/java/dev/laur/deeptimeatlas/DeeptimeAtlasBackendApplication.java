db7c4d4b7f7f8b9121b7c454ded412f461f88b04
laurinho <ioan-laurentiu.nistor@student.unitbv.ro>
2026-09-21 12:49:00 +0300

    chore: initialize Spring Boot backend

---
+++ backend/src/main/java/dev/laur/deeptimeatlas/DeeptimeAtlasBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.laur.deeptimeatlas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeeptimeAtlasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeeptimeAtlasBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/laur/deeptimeatlas/DeeptimeAtlasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.laur.deeptimeatlas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeeptimeAtlasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
